bdf63c86f1f7a57c94eb6aaa49d3d088de1c8bc9
Mayur Adlak <adlak.mayur@Webseeder.in>
2023-02-17 12:11:55 +0530

    Footer Modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -31,37 +31,8 @@ const NewFooter = () => {
         <div className="text-black">
           <section className="mb-4">
             </section>
-          {/* 
-          <section className="">
-            <form action="">
-              <div className="row d-flex justify-content-center">
-                <div className="col-auto">
-                  <p className="pt-2">
-                    <strong>Sign up for our new Upcoming Trip</strong>
-                  </p>
-                </div>
-
-                <div className="col-md-5 col-12">
-                  <div className="form-outline form-white mb-4">
-                    <input
-                      type="email"
-                      id="form5Example21"
-                      className="form-control"
-                    />
-                    <label className="form-label" for="form5Example21">
-                      Email address
-                    </label>
-                  </div>
-                </div>
-                <div className="col-auto">
-                  <button type="submit" className="btn btn-outline-light mb-4">
-                    Subscribe
-                  </button>
-                </div>
-              </div>
-            </form>
-          </section>
-           */}
+        
+          
           <section className="main bg-white">
             <div className="row">
               <div className="col-lg-3 col-md-6 mb-4 text-start" id="logo-container">
