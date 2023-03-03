7eddbe84041ec63af613a4a35c1d6460c3de80d7
Mayank Joshi <mayank1305.joshi@gmail.com>
2023-03-03 10:16:41 +0530

    add event page

--- src/Components/event/Event.jsx
+++ src/Components/event/Event.jsx
@@ -5,7 +5,14 @@ import "./event.css";
 const Event = () => {
   return (
     <>
-      <Slider />;
+      <div>
+        A Comprehensive Platform For
+        <br></br>
+        <b>Your Shows & Events Lectus duis tortor</b>
+        vitae enim ultrices tincidunt quam euismod tincidunt enim id commodo
+        Lectus duis.
+        <button>FIND YOUR NEXT EVENT</button>
+      </div>
     </>
   );
 };
